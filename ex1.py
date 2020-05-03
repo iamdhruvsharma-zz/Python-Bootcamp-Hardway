627889a7dbc246a5b56e189f8caa2872255d92f7
Dhruv Sharma <dhruv061@hotmail.com>
2020-05-03 14:57:07 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 14:41:41 2020
+
+@author: DXT
+"""
+
+#print("Hello World!")
+#print("Hello Again")
+#print("I like typing this.")
+print("This is fun.")
+#print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+#print("This my orignal line by DXT")
