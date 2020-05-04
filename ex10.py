0a1ef6a0261dee3bdd68bcf7f5a00a4608a8fa21
Dhruv Sharma <dhruv061@hotmail.com>
2020-05-04 14:44:56 +0530

    Add files via upload

--- ex10.py
+++ ex10.py
@@ -25,20 +25,20 @@ print(backslash_cat_equivalent)
 print(fat_cat)
 
 
-"""
-Escape .
-\\      Backslash (\)
-\'      Single-quote (')
-\"      Double-quote (")
-\a      ASCII bell (BEL)
-\b      ASCII backspace (BS)
-\f      ASCII formfeed (FF)
-\n      ASCII linefeed (LF)
-\N{name}        Character named name in the Unicode database (Unicode only)
-\r      Carriage return (CR)
-\t      Horizontal tab (TAB)
-\uxxxx      Character with 16-bit hex value xxxx
-\Uxxxxxxxx      Character with 32-bit hex value xxxxxxxx
-\v      ASCII vertical tab (VT)
-\000        Character with octal value 000
-\xhh        Character with hex value hh
+
+# \\      Backslash (\)
+# \'      Single-quote (')
+# \"      Double-quote (")
+# \a      ASCII bell (BEL)
+# \b      ASCII backspace (BS)
+# \f      ASCII formfeed (FF)
+# \n      ASCII linefeed (LF)
+# \N{name}        Character named name in the Unicode database (Unicode only)
+# \r      Carriage return (CR)
+# \t      Horizontal tab (TAB)
+# \uxxxx      Character with 16-bit hex value xxxx
+# \Uxxxxxxxx      Character with 32-bit hex value xxxxxxxx
+# \v      ASCII vertical tab (VT)
+# \000        Character with octal value 000
+# \xhh        Character with hex value hh
+
