2728b21f324fc785cc9153f66a575d9e59a2ff77
Dhruv Sharma <dhruv061@hotmail.com>
2020-05-04 14:40:54 +0530

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 22:54:16 2020
+
+@author: DXT
+"""
+# \t - TAB
+tabby_cat = "\tI'm tabbed in."
+# \n - new line
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+backslash_cat_equivalent = "I'm \ a \ cat."
+
+fat_cat = """
+I'll do a list:
+\t-> Cat food
+\t-> Fishies
+\t-> Catnip\n\t-> Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(backslash_cat_equivalent)
+print(fat_cat)
+
+
+"""
+Escape .
+\\      Backslash (\)
+\'      Single-quote (')
+\"      Double-quote (")
+\a      ASCII bell (BEL)
+\b      ASCII backspace (BS)
+\f      ASCII formfeed (FF)
+\n      ASCII linefeed (LF)
+\N{name}        Character named name in the Unicode database (Unicode only)
+\r      Carriage return (CR)
+\t      Horizontal tab (TAB)
+\uxxxx      Character with 16-bit hex value xxxx
+\Uxxxxxxxx      Character with 32-bit hex value xxxxxxxx
+\v      ASCII vertical tab (VT)
+\000        Character with octal value 000
+\xhh        Character with hex value hh
---
+++ ex9.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 22:49:11 2020
+
+@author: DXT
+"""
+
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# \n takes the cursor to new next line
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
